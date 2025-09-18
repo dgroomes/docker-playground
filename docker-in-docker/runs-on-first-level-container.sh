e9cbe01bd3864a7059c530077dcf3748caad644a
David Groomes <dwgroomes@gmail.com>
2025-09-18 18:14:27 -0500

    Custom Docker-in-Docker setup with Debian (#1)

--- docker-in-docker/runs-on-first-level-container.sh
+++ docker-in-docker/runs-on-first-level-container.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # This is designed to run in the 'first level' container. It builds and runs its own Docker container.
 
-echo "Hello from $(hostname)" >> /app/log.txt
+echo "(first-level-container) Hello from $(hostname)" >> /app/log.txt
 
-echo "Waiting for Docker daemon to be ready..."
+echo "(first-level-container) Waiting for Docker daemon to be ready..."
 i=1
 while [ $i -le 30 ]; do
     if docker version >/dev/null 2>&1; then
-        echo "Docker daemon is ready!"
+        echo "(first-level-container) Docker daemon is ready!"
         break
     fi
     if [ $i -eq 30 ]; then
-        echo "Docker daemon failed to start within 30 seconds"
+        echo "(first-level-container) Docker daemon failed to start within 30 seconds"
         exit 1
     fi
     echo "Attempt $i/30..."
@@ -19,6 +19,8 @@ while [ $i -le 30 ]; do
     i=$((i + 1))
 done
 
-docker build --file /app/Dockerfile.nested --tag docker-playground-docker-in-docker-nested /app
+echo "(first-level-container) Building the nested container image ..."
+docker build --file /app/Dockerfile.nested --tag docker-playground-docker-in-docker-nested --quiet /app
 
+echo "(first-level-container) Running the nested container ..."
 docker run --rm --mount type=bind,source=/app,target=/app docker-playground-docker-in-docker-nested
--- docker-in-docker/runs-on-nested-container.sh
+++ docker-in-docker/runs-on-nested-container.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # This is designed to run in the nested container.
 
-echo "Hello from $(hostname)" >> /app/log.txt
+echo "(nested-container) Hello from $(hostname)" >> /app/log.txt
